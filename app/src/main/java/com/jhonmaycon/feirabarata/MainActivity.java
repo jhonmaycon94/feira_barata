5341dbb6d9790c0f944e447c3d4cdf067fad04fb
Jhon Maycon <jhon.maycon@hotmail.com>
2019-10-16 16:47:14 -0300

    add menu and a intent to start login activity

--- app/src/main/java/com/jhonmaycon/feirabarata/MainActivity.java
+++ app/src/main/java/com/jhonmaycon/feirabarata/MainActivity.java
@@ -3,7 +3,11 @@ package com.jhonmaycon.feirabarata;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar appToolbar = findViewById(R.id.app_toolbar);
         setSupportActionBar(appToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_login:
+                Intent startLoginActivity = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(startLoginActivity);
+                return true;
+            case R.id.menu_cadastro:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
