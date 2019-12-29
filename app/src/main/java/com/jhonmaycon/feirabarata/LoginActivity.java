2ec5565c6c28deea52642fbb236706fd000c0bfe
jhon maycon <jhonmaycon94@gmail.com>
2019-12-29 20:36:13 -0300

    init login backend

--- app/src/main/java/com/jhonmaycon/feirabarata/LoginActivity.java
+++ app/src/main/java/com/jhonmaycon/feirabarata/LoginActivity.java
@@ -1,12 +1,71 @@
 package com.jhonmaycon.feirabarata;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
-public class LoginActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button btnLogin;
+    private ProgressBar loading;
+    private static String URL_LOGIN = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login); }
+        setContentView(R.layout.activity_login);
+
+        loading = findViewById(R.id.loading);
+        btnLogin = findViewById(R.id.btn_login);
+        email = findViewById(R.id.edtEmail);
+        password = findViewById(R.id.edt_senha);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mEmail = email.getText().toString().trim();
+                String mPassword = password.getText().toString().trim();
+
+                if(!mEmail.isEmpty() || !mPassword.isEmpty()){
+                    //login(mEmail, mLogin);
+                }
+                else{
+                    email.setError("Por favor, insira o e-mail!");
+                    password.setError("Por favor, insira sua senha");
+                }
+            }
+        });
+    }
+
+public void login(String email, String senha){
+        loading.setVisibility(View.VISIBLE);
+
+    StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN, new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+
+        }
+    }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+
+        }
+    })
+    {
+        
+    }
+}
+
 }
