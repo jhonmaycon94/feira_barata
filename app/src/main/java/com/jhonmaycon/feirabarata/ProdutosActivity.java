241653e8b73549eb464fbb5ee749c72cb5697e58
jhon maycon <jhonmaycon94@gmail.com>
2019-12-31 17:14:48 -0300

    create backend products view

--- app/src/main/java/com/jhonmaycon/feirabarata/ProdutosActivity.java
+++ app/src/main/java/com/jhonmaycon/feirabarata/ProdutosActivity.java
@@ -1,5 +1,6 @@
 package com.jhonmaycon.feirabarata;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
@@ -14,9 +15,20 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
-public class ProdutosActivity extends AppCompatActivity {
+public class ProdutosActivity extends AppCompatActivity implements ProdutosRecyclerViewAdapter.OnProductCardViewListener {
 
     private static final String TAG = "ProdutosActivity";
 
@@ -24,6 +36,9 @@ public class ProdutosActivity extends AppCompatActivity {
     private ArrayList<String> productsDescription = new ArrayList<>();
     private ArrayList<String> productsPrice = new ArrayList<>();
     private String productsSupermercado;
+    private String supermercadoId;
+    private String URL_DATA;
+    SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +47,29 @@ public class ProdutosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_produtos);
         Toolbar appToolbar = findViewById(R.id.app_toolbar);
         setSupportActionBar(appToolbar);
-        loadImagesBitmap();
+
+        Intent intent = getIntent();
+        supermercadoId = intent.getStringExtra("supermercadoId");
+
+        URL_DATA = "http://feirabarata.jhonmaycon.com/api/products/read.php?supermercadoId="+supermercadoId;
+
+        sessionManager = new SessionManager(this);
+        getProducts();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem logOut = menu.findItem(R.id.menu_logout);
+        MenuItem logIn = menu.findItem(R.id.menu_login);
+        MenuItem cadastro = menu.findItem(R.id.menu_cadastro);
+        MenuItem produto = menu.findItem(R.id.menu_produto);
+
+        logOut.setVisible(sessionManager.isLoged());
+        produto.setVisible(sessionManager.isLoged());
+        logIn.setVisible(!(sessionManager.isLoged()));
+        cadastro.setVisible(!(sessionManager.isLoged()));
+
+        return true;
     }
 
     @Override
@@ -54,12 +91,59 @@ public class ProdutosActivity extends AppCompatActivity {
                 Intent startCadastroActivity = new Intent(ProdutosActivity.this, CadastroActivity.class);
                 ProdutosActivity.this.startActivity(startCadastroActivity);
                 return true;
+            case R.id.menu_logout:
+                sessionManager.logout();
+            case R.id.menu_produto:
+               /* Intent startNovoProdutoActivity = new Intent(ProdutosActivity.this, NovoProduto.class);
+                startNovoProdutoActivity.putExtra("user_email", emailsession);
+                startNovoProdutoActivity.putExtra("supermercadosNames", supermercadoName);
+                startNovoProdutoActivity.putExtra("supermercadosId", supermercadoId);
+                ProdutosActivity.this.startActivity(startNovoProdutoActivity);*/
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    public void loadImagesBitmap(){
+    public void getProducts(){
+        Log.d(TAG, "getProducts: called");
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Carregando dados...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_DATA, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+                Log.d(TAG, "onResponse: "+response);
+                try {
+                    JSONObject productsListJson = new JSONObject(response);
+                    JSONArray records = productsListJson.getJSONArray("records");
+
+                    for(int i=0; i < records.length(); i++){
+                        JSONObject product = records.getJSONObject(i);
+                        productsImagesURL.add(product.getString("foto"));
+                        productsPrice.add(product.getString("preco"));
+                        String productDescription = product.getString("nome").toUpperCase() + " " + product.getString("marca").toUpperCase()+ " " + product.getString("quantidade");
+                        productsDescription.add(productDescription);
+                    }
+                }
+                catch (JSONException e){
+                    Log.d(TAG, "onResponse: "+e.getMessage());
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                Log.d(TAG, "onErrorResponse: "+error.getMessage());
+            }
+        });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);
+
         initRecyclerView();
     }
 
@@ -67,10 +151,15 @@ public class ProdutosActivity extends AppCompatActivity {
         Log.d(TAG, "initrecyclerView: called");
 
         RecyclerView recyclerView = findViewById(R.id.produtos_reclycler_view);
-        ProdutosRecyclerViewAdapter adapter = new ProdutosRecyclerViewAdapter(productsDescription, productsPrice, productsImagesURL, productsSupermercado, this);
+        ProdutosRecyclerViewAdapter adapter = new ProdutosRecyclerViewAdapter(productsDescription, productsPrice, productsImagesURL, productsSupermercado, this, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), recyclerView.getLayoutManager().getLayoutDirection());
         recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    @Override
+    public void onProductCardViewClick(int position) {
+
     }
 }
--- app/src/main/java/com/jhonmaycon/feirabarata/ProdutosRecyclerViewAdapter.java
+++ app/src/main/java/com/jhonmaycon/feirabarata/ProdutosRecyclerViewAdapter.java
@@ -9,7 +9,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -24,20 +23,22 @@ public class ProdutosRecyclerViewAdapter extends RecyclerView.Adapter<ProdutosRe
     private ArrayList<String> productsImagesList = new ArrayList<>();
     private String productsSupermercado;
     private Context context;
+    OnProductCardViewListener monProductCardViewListener;
 
-    public ProdutosRecyclerViewAdapter(ArrayList<String> productsDescriptionList, ArrayList<String> productsPriceList, ArrayList<String> productsImagesList, String productsSupermercado, Context context) {
+    public ProdutosRecyclerViewAdapter(ArrayList<String> productsDescriptionList, ArrayList<String> productsPriceList, ArrayList<String> productsImagesList, String productsSupermercado, Context context, OnProductCardViewListener monProductCardViewListener) {
         this.productsDescriptionList = productsDescriptionList;
         this.productsPriceList = productsPriceList;
         this.productsImagesList = productsImagesList;
         this.productsSupermercado = productsSupermercado;
         this.context = context;
+        this.monProductCardViewListener = monProductCardViewListener;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.produtos_item_view, parent, false);
-        ViewHolder holder = new ViewHolder(view);
+        ViewHolder holder = new ViewHolder(view, monProductCardViewListener);
         return holder;
     }
 
@@ -45,40 +46,44 @@ public class ProdutosRecyclerViewAdapter extends RecyclerView.Adapter<ProdutosRe
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: called");
 
-        Glide.with(context).asBitmap().load(productsImagesList.get(position)).into(holder.productImage);
+        //Glide.with(context).asBitmap().load(productsImagesList.get(position)).into(holder.productImage);
         holder.productSupermercado.setText(productsSupermercado);
         holder.prdocutDescription.setText(productsDescriptionList.get(position));
         holder.prdocutPrice.setText(productsPriceList.get(position));
-        holder.productsLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG,"onClick: clicked on: " + productsDescriptionList.get(position));
-            }
-        });
-
     }
 
     @Override
     public int getItemCount() {
-        return productsDescriptionList.size();
+        return productsPriceList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView productImage;
         TextView prdocutDescription;
         TextView productSupermercado;
         TextView prdocutPrice;
-        ConstraintLayout productsLayout;
+        OnProductCardViewListener onProductCardViewListener;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView, OnProductCardViewListener onProductCardViewListener) {
             super(itemView);
 
             productImage = itemView.findViewById(R.id.product_image);
             prdocutDescription =itemView.findViewById(R.id.product_description);
             prdocutPrice = itemView.findViewById(R.id.product_price);
             productSupermercado = itemView.findViewById(R.id.product_supermercado);
-            productsLayout = itemView.findViewById(R.id.produtos_item_layout);
+            this.onProductCardViewListener = onProductCardViewListener;
+
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            onProductCardViewListener.onProductCardViewClick(getAdapterPosition());
+        }
+    }
+
+    public interface OnProductCardViewListener {
+        void onProductCardViewClick(int position);
     }
 }
